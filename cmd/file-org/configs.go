b931f2d1ac9d49fffcd21502d1abf9841c5e23db
AYehia0 <ahmedyehia611@gmail.com>
2023-01-04 15:24:37 +0200

    Fixed a typo in extension name: str -> str

--- cmd/file-org/configs.go
+++ cmd/file-org/configs.go
@@ -3,7 +3,7 @@ package fileorg
 /*
 This file contains all the different types of files and the directory corresponding to the file type.
 */
-var DOC_FILES = []string{"txt", "pdf", "doc", "xml", "md", "xlsx", "csv", "py", "info", "log", "css", "str"}
+var DOC_FILES = []string{"txt", "pdf", "doc", "xml", "md", "xlsx", "csv", "py", "info", "log", "css", "srt"}
 var VID_FILES = []string{"mp4", "avi", "flv", "mkv", "mov", "webm"}
 var RAR_FILES = []string{"rar", "7z", "zip", "gz", "tar", "iso"}
 var IMG_FILES = []string{"png", "jpg", "jpeg", "tif", "gif", "bmp", "webp"}
